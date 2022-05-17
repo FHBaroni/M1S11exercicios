86f0b9b53a045c6954a99a9a7725234702256b68
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-05-17 15:25:26 -0300

    Exercicio 05 semana 11 curso DevInHouse[NDD]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { M1S11ex2Component } from '../app/exercicios/m1-s11ex2/m1-s11ex2.compone
 import { AppComponent } from './app.component';
 import { M1s11ex3Component } from './exercicios/m1s11ex3/m1s11ex3.component';
 import { M1s11ex4Component } from './exercicios/m1s11ex4/m1s11ex4.component';
+import { M1s11ex5Component } from './exercicios/m1s11ex5/m1s11ex5.component';
 
 @NgModule({
-  declarations: [AppComponent, M1S11ex2Component, M1s11ex3Component, M1s11ex4Component],
+  declarations: [
+    AppComponent,
+    M1S11ex2Component,
+    M1s11ex3Component,
+    M1s11ex4Component,
+    M1s11ex5Component,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/exercicios/m1s11ex5/m1s11ex5.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-m1s11ex5',
+  templateUrl: './m1s11ex5.component.html',
+  styleUrls: ['./m1s11ex5.component.css'],
+})
+export class M1s11ex5Component implements OnInit {
+  constructor() {}
+  showModal: boolean = true;
+  ngOnInit(): void {}
+}
