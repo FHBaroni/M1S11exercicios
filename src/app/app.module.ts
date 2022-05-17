a41f29e8c8159c9186059b643c5ed5dd1671f058
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-05-17 12:10:50 -0300

    Exercicio 03 semana 11 curso DevInHouse[NDD]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { M1S11ex2Component } from '../app/exercicios/m1-s11ex2/m1-s11ex2.component';
 import { AppComponent } from './app.component';
+import { M1s11ex3Component } from './exercicios/m1s11ex3/m1s11ex3.component';
 
 @NgModule({
-  declarations: [AppComponent, M1S11ex2Component],
+  declarations: [AppComponent, M1S11ex2Component, M1s11ex3Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/exercicios/m1s11ex3/m1s11ex3.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-m1s11ex3',
+  templateUrl: './m1s11ex3.component.html',
+  styleUrls: ['./m1s11ex3.component.css'],
+})
+export class M1s11ex3Component implements OnInit {
+  arrayList: string[] = [
+    'Matrix ',
+    'Clube da luta ',
+    'Taxi driver ',
+    'coringa ',
+    'Logan ',
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
