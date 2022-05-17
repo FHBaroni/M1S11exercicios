d033b7de21c3393acbc5e9b0a89606fb9e9f0924
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-05-17 00:50:38 -0300

    Exercicio 02 semana 11 curso DevInHouse[NDD]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { M1S11ex2Component } from '../app/exercicios/m1-s11ex2/m1-s11ex2.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, M1S11ex2Component],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/exercicios/m1-s11ex2/m1-s11ex2.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-m1-s11ex2',
+  templateUrl: './m1-s11ex2.component.html',
+  styleUrls: ['./m1-s11ex2.component.css'],
+})
+export class M1S11ex2Component implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+  modeOn: boolean = false;
+}
